25be7cc8c7c9d02a9b4b5ef9f6e93407b7bd026e
Lukasz Krason <l.krason91@gmail.com>
2020-09-06 14:21:01 +0200

    Wykonano testy dla zadanego kodu

--- src/main/java/pl/sda/calculator/Calculator.java
+++ src/main/java/pl/sda/calculator/Calculator.java
@@ -3,6 +3,41 @@ package pl.sda.calculator;
 public class Calculator {
 
     public int add(int arg1, int arg2) {
+
         return arg1 + arg2;
     }
+
+    public boolean isDividableByThree(int arg) {
+
+        return arg % 3 == 0;
+    }
+
+    public int substract(int arg1, int arg2) {
+        return arg1 - arg2;
+    }
+
+    public int multiplication1(int arg1, int arg2) {
+        return arg1 * arg2;
+    }
+
+    public int decrementation(int arg1, int arg2) {
+        if (arg2 == 0) {
+            return 0;
+        } else {
+            return arg1 / arg2;
+        }
+
+    }
+
+    public boolean isEven(int arg1) {
+        return (arg1 % 2) == 0;
+    }
+    public boolean isNotEven(int arg1) {
+        return (arg1 % 2) != 0;
+    }
+    public double percentage(double arg1, double arg2) {
+//        throw new RuntimeException("Nie zaimplementowano");
+        return (arg1 * arg2) / 100;
+    }
+
 }
--- src/test/java/pl/sda/calculator/CalculatorTest.java
+++ src/test/java/pl/sda/calculator/CalculatorTest.java
@@ -3,7 +3,7 @@ package pl.sda.calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
@@ -21,5 +21,181 @@ class CalculatorTest {
 
         //then
         assertEquals(5.0, actual);
+    }
+
+    @Test
+    void shouldBeDividableByThree() {
+        //given
+        int arg = 9;
+
+        // when
+        boolean actual = sut.isDividableByThree(arg);
+
+        //then
+        assertTrue(actual);
+    }
+
+    @Test
+    @DisplayName("Powinnno zwracac 14 przy odejmowaniu 7 od 21")
+    void shouldReturnFourteenForGivenArgsOnSubstractionOperation() {
+        // given
+        int arg1 = 21;
+        int arg2 = 7;
+
+        //when
+        var actual = sut.substract(arg1, arg2);
+
+        //then
+        assertEquals(14, actual);
+    }
+
+    @Test
+    @DisplayName("Powinnno zwracac 10 przy mnożeniu 2 razy 5")
+    void shouldReturnTenForTwoTimesFive() {
+        // given
+        int arg1 = 2;
+        int arg2 = 5;
+
+        //when
+        int actual = sut.multiplication1(arg1, arg2);
+
+        //then
+        assertEquals(10, actual);
+    }
+
+    @Test
+    @DisplayName("Powinnno zwracac 0 przy mnożeniu razy 0")
+    void shouldReturnZeroForMultiplicationZero() {
+        // given
+        int arg1 = 0;
+        int argNotImportant = 859;
+
+        //when
+        int actual = sut.multiplication1(arg1, argNotImportant);
+
+        //then
+        assertEquals(0, actual);
+    }
+
+    @Test
+    @DisplayName("Powinnno zwracac 2 przy dzieleniu 6 przez 3")
+    void shouldReturnTwoForDecrementationSixPerThree() {
+        // given
+        int arg1 = 6;
+        int arg2 = 3;
+
+        //when
+        int actual = sut.decrementation(arg1, arg2);
+
+        //then
+        assertEquals(2, actual);
+    }
+
+    @Test
+    @DisplayName("Powinnno zwracac 0 przy dzieleniu 0 przez liczbę")
+    void shouldReturnZeroForDecrementationZeroPerValue() {
+        // given
+        int arg1 = 0;
+        int arg2 = 3;
+
+        //when
+        int actual = sut.decrementation(arg1, arg2);
+
+        //then
+        assertEquals(0, actual);
+    }
+
+    @Test
+    @DisplayName("Powinnno zwracac 0 przy dzieleniu przez 0")
+    void shouldReturnZeroForDecrementationZero() {
+        // given
+        int argNotImportant = 2587;
+        int arg2 = 0;
+
+        //when
+        int actual = sut.decrementation(argNotImportant, arg2);
+
+        //then
+        assertEquals(0, actual);
+    }
+
+    @Test
+    @DisplayName("Powinnno zwracac true dla liczby parzystej")
+    void shouldReturnTrueForEvenValue() {
+        // given
+        int arg = 6;
+
+        //when
+        boolean actual = sut.isEven(arg);
+
+        //then
+        assertTrue(actual);
+    }
+
+    @Test
+    @DisplayName("Powinnno zwracac false dla liczby nieparzystej")
+    void shouldReturnFalseForNotValue() {
+        // given
+        int arg = 5;
+
+        //when
+        boolean actual = sut.isEven(arg);
+
+        //then
+        assertFalse(actual);
+    }
+
+    @Test
+    @DisplayName("Powinnno zwracac true dla liczby nieparzystej")
+    void shouldReturnTrueForNotEvenValue() {
+        // given
+        int arg = 11;
+
+        //when
+        boolean actual = sut.isNotEven(arg);
+
+        //then
+        assertTrue(actual);
+    }
+
+    @Test
+    @DisplayName("Powinnno zwracac false dla liczby parzystej")
+    void shouldReturnFalseForEvenValue() {
+        // given
+        int arg = 6;
+
+        //when
+        boolean actual = sut.isNotEven(arg);
+
+        //then
+        assertFalse(actual);
+    }
+
+    @Test
+    @DisplayName("Powinnno zwracac 0.5 dla 5 % z 10")
+    void shouldReturnProperePercentage() {
+        // given
+        double arg1 = 10.0d;
+        double arg2 = 5.0d;
+
+        //when
+        double actual = sut.percentage(arg1,arg2);
+
+        //then
+        assertEquals(0.5d, actual,0.000001d);
+    }
+
+    @Test
+    @DisplayName("Powinnno zwracac 200 dla 10 % z 2000")
+    void shouldReturnProperePercentageForIntegerNumber() {
+        // given
+        double arg1 = 2000d;
+        double arg2 = 10d;
+
+        //when
+        double actual = sut.percentage(arg1,arg2);
+
+        //then
+        assertEquals(200, actual,0.000001d);
     }
 }
